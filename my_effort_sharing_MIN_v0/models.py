28ac1f553921483f4867f2d3c61f1b7b3dc7b6eb
sugakensho <n.pride.bluemiracle@gmail.com>
2020-12-16 12:19:40 +0900

    MIN-v0を更新

--- my_effort_sharing_MIN_v0/models.py
+++ my_effort_sharing_MIN_v0/models.py
@@ -20,7 +20,7 @@ doc = """
 class Constants(BaseConstants):
     name_in_url = 'my_effort_sharing_MIN_v0'
     players_per_group = 4
-    num_rounds = 5
+    num_rounds = 15
 
     max_effort = (100)
 
@@ -29,8 +29,6 @@ class Constants(BaseConstants):
     reward_low = 40
     reward_even = 50
     reward_high = 140
-    endowment1 = 40
-    endowment2 = 0
 
 
 class Subsession(BaseSubsession):
@@ -50,29 +48,47 @@ class Group(BaseGroup):
         p3 = self.get_player_by_id(3)
         p4 = self.get_player_by_id(4)
 
-        p1.yeild = p1.effort_amount + Constants.endowment1 +  random.uniform(-50,50) 
-        p2.yeild = p2.effort_amount + Constants.endowment1 +  random.uniform(-50,50) 
-        p3.yeild = p3.effort_amount + Constants.endowment2 +  random.uniform(-50,50) 
-        p4.yeild = p4.effort_amount + Constants.endowment2 +  random.uniform(-50,50) 
-
         team_type = random.randint(1,2)
 
         if team_type == 1 :
+            p1.demand_shock = random.uniform(-50,50) 
+            p2.demand_shock = random.uniform(-50,50)
+            p3.demand_shock = random.uniform(-50,50)
+            p4.demand_shock = random.uniform(-50,50)
+
+            p1.endowment = 40
+            p2.endowment = 40
+            p3.endowment = 0
+            p4.endowment = 0
+
+
+            p1.yeild = p1.effort_amount + p1.endowment +  p1.demand_shock
+            p2.yeild = p2.effort_amount + p2.endowment +  p2.demand_shock
+            p3.yeild = p3.effort_amount + p3.endowment +  p3.demand_shock
+            p4.yeild = p4.effort_amount + p4.endowment +  p4.demand_shock
+
             team1_yeild = min(p1.yeild,p3.yeild)
             team2_yeild = min(p2.yeild,p4.yeild)
+        
             p1.team = 1
-            p1.team_members_yeild = p3.yeild
             p2.team = 2
-            p2.team_members_yeild = p4.yeild
             p3.team = 1
-            p3.team_members_yeild = p1.yeild
             p4.team = 2
-            p4.team_members_yeild = p2.yeild
+
             p1.team_member = 3
             p2.team_member = 4
             p3.team_member = 1
             p4.team_member = 2
 
+            p1.team_members_yeild = p3.yeild
+            p2.team_members_yeild = p4.yeild
+            p3.team_members_yeild = p1.yeild
+            p4.team_members_yeild = p2.yeild
+
+            p1.effort_sharing_amount = p1.effort_amount*0
+            p2.effort_sharing_amount = p2.effort_amount*0
+            p3.effort_sharing_amount = p1.effort_amount*0
+            p4.effort_sharing_amount = p2.effort_amount*0
 
             if team1_yeild > team2_yeild :
                 p1.payoff = Constants.reward_high - p1.effort_amount * p1.effort_amount * 0.01
@@ -93,20 +109,43 @@ class Group(BaseGroup):
                 p4.payoff = Constants.reward_high - p4.effort_amount * p4.effort_amount * 0.01
 
         else :
+            p1.demand_shock = random.uniform(-50,50) 
+            p2.demand_shock = random.uniform(-50,50)
+            p3.demand_shock = random.uniform(-50,50)
+            p4.demand_shock = random.uniform(-50,50)
+
+            p1.endowment = 40
+            p2.endowment = 40
+            p3.endowment = 0
+            p4.endowment = 0
+
+            p1.yeild = p1.effort_amount + p1.endowment +  p1.demand_shock
+            p2.yeild = p2.effort_amount + p2.endowment +  p2.demand_shock
+            p3.yeild = p3.effort_amount + p3.endowment +  p3.demand_shock 
+            p4.yeild = p4.effort_amount + p4.endowment +  p4.demand_shock 
+            
             team1_yeild = min(p1.yeild,p4.yeild)
             team2_yeild = min(p2.yeild,p3.yeild)
+
             p1.team = 1
-            p1.team_members_yeild = p4.yeild
             p2.team = 2
-            p2.team_members_yeild = p3.yeild
             p3.team = 2
-            p3.team_members_yeild = p2.yeild
             p4.team = 1
-            p4.team_members_yeild = p1.yeild
+
             p1.team_member = 4
             p2.team_member = 3
             p3.team_member = 2
             p4.team_member = 1
+
+            p1.team_members_yeild = p4.yeild
+            p2.team_members_yeild = p3.yeild
+            p3.team_members_yeild = p2.yeild
+            p4.team_members_yeild = p1.yeild
+
+            p1.effort_sharing_amount = p1.effort_amount*0
+            p2.effort_sharing_amount = p2.effort_amount*0
+            p3.effort_sharing_amount = p2.effort_amount*0
+            p4.effort_sharing_amount = p1.effort_amount*0
 
             if team1_yeild > team2_yeild :
                 p1.payoff = Constants.reward_high - p1.effort_amount * p1.effort_amount * 0.01
@@ -130,6 +169,9 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
     effort_amount = models.IntegerField(min=0, max=100)
+    effort_sharing_amount = models.FloatField(initial=0)
+    demand_shock = models.FloatField(initial=0)
+    endowment =  models.IntegerField(initial=0)
     yeild = models.FloatField(initial=0)
 
     team = models.IntegerField(initial=0)
